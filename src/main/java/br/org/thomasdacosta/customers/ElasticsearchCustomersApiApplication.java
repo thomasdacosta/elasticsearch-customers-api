ab0cf113ea7c4ba9dc9976a896c545a485d6f4cb
Thomás da Costa <thomasdacosta@gmail.com>
2022-11-27 21:15:36 -0300

    first commit

---
+++ src/main/java/br/org/thomasdacosta/customers/ElasticsearchCustomersApiApplication.java
@@ -0,0 +1,13 @@
+package br.org.thomasdacosta.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchCustomersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchCustomersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/thomasdacosta/customers/ElasticsearchCustomersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.thomasdacosta.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchCustomersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
